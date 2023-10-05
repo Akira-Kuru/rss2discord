0809e922aecd3247f23ce36f146b3db1c504dcfb
Pedro Bauer <pgm.bauer@gmail.com>
2023-10-05 11:47:32 +0200

    Moving Repos to avoid Data Leak

---
+++ readme.md
@@ -0,0 +1,10 @@
+# RSS2Discord
+
+RSS2Discord is an approach to send an RSS Feed to a Discord Webhook. Idea here is to have a constant update of statuses to but pushed to the Discord Webhook.
+
+In this example we would be using nitter.net which is an alternative to twitter.com / x.com and to get tweets pushed on to Discord.
+
+### Inspiration
+This project was inspired by the Tweetshift Discord Bot. Tweetshift is a Discord bot that got tweets from Twitter.com and posted them on any specified Discord channel. As Tweetshift changed their usage only for paid users and the free approach had been discontinued. I took it upon myself to rebuild this functionality with a much minimalistic approach.
+
+Currently the RSS2Discord embed looks very similar to the Tweetshift approach and excludes Retweets as the Tweetshift bot also did by default.
---
+++ src/index.ts
@@ -0,0 +1,67 @@
+import RSSParser from 'rss-parser';
+import schedule from 'node-schedule';
+import { EmbedBuilder, WebhookClient } from 'discord.js';
+import axios from 'axios'
+import dotenv from 'dotenv';
+
+dotenv.config({});
+
+const discordWebhookUrl = process.env.DISCORD_WEBHOOK!;
+const rssFeedUrl = 'https://' + process.env.RSS_DOMAIN! + '/' + process.env.RSS_USERNAME! + '/rss';
+const webhook = new WebhookClient({ url: discordWebhookUrl });
+
+const job = schedule.scheduleJob('*/10 * * * * *', () => {
+  runTask();
+});
+
+async function runTask() {
+  const feed = await fetchRSSFeed(rssFeedUrl);
+  await sendToWebhook(feed);
+}
+
+async function fetchRSSFeed(rssFeedUrl: string) {
+  try {
+    const response = await axios(rssFeedUrl);
+    const parser = new RSSParser();
+    const feed = await parser.parseString(await response.data);
+    return feed;
+  } catch (error) {
+    console.error('Error fetching or delivering the RSS feed:', error);
+  }
+}
+
+async function sendToWebhook(feed: any) {
+  try {
+    const latestItem = feed?.items[0];
+    let embed = new EmbedBuilder()
+      .setColor(0x0099FF)
+      .setURL(latestItem?.link?.replace('#m', '').replace(process.env.RSS_DOMAIN!, 'x.com') as string)
+      .setAuthor({ name: feed?.image?.title as string, iconURL: feed?.image?.url, url: `https://x.com/${process.env.RSS_USERNAME!}` })
+      .setDescription(latestItem?.content
+        ?.replace(/<\/(div|p|h[1-6])>/g, '$&\n\n') // Add double line break after closing tags for div, p, and heading tags
+        ?.replace(/(<(br|\/li|\/ul|\/ol)>)+/g, '\n') // Replace br, closing li, closing ul, and closing ol tags with a newline
+        .replace(/(<([^>]+)>)/gi, '') as string)
+      .setTimestamp()
+      .setFooter({ text: process.env.DISCORD_BOT_NAME!, iconURL: process.env.DISCORD_BOT_ICON! });
+
+    if (latestItem?.content?.match('<img')) {
+      const match = latestItem?.content?.match(/<img.*?src="(.*?)"/);
+      embed = embed.setImage(match ? match[1] : '');
+    }
+
+    if (!latestItem?.title?.match('RT by')) {
+      webhook.send({
+        content: `[Tweeted](${latestItem?.link?.replace('#m', '').replace(process.env.RSS_DOMAIN!, 'x.com')})`,
+        username: process.env.DISCORD_BOT_NAME!,
+        avatarURL: process.env.DISCORD_BOT_ICON!,
+        embeds: [embed]
+      });
+      console.log(embed);
+      console.log('RSS feed delivered to Discord webhook successfully!');
+    } else {
+      console.log('its a retweet');
+    }
+  } catch (error) {
+    console.error('Error sending to Discord Webhook:', error);
+  }
+}
